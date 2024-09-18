a3aafe5e5446bb1ac0360dd5d53ff228d6364218
Juan Esteban Cardona Ospina <juan.cardona@ibox.com.co>
2024-09-18 10:52:17 -0500

    login and supressionList page

--- src/app/dashboard/components/sidebar/SideBar.tsx
+++ src/app/dashboard/components/sidebar/SideBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Drawer,
   List,
@@ -9,15 +10,10 @@ import {
   Collapse,
   styled,
 } from "@mui/material";
-import {
-  ExpandMore,
-  Email,
-  Assessment,
-  Settings,
-  Notifications,
-} from "@mui/icons-material";
+import { ExpandMore } from "@mui/icons-material";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import { MenuItemProps } from "./interfaces";
+import { MenuItemData } from "./interfaces";
+import { menuItems } from "./data";
 
 const drawerWidth = "276px";
 
@@ -37,46 +33,16 @@ const NestedListItem = styled(ListItem)(({ theme }) => ({
   paddingLeft: theme.spacing(4),
 }));
 
-const menuItems: MenuItemProps[] = [
-  {
-    title: "Email Tools",
-    icon: <Email />,
-    subItems: [{ title: "Campaign", content: <div>Campaign Section</div> }],
-  },
-  {
-    title: "Reports",
-    icon: <Assessment />,
-    subItems: [
-      {
-        title: "Sending Overview",
-        content: <div>Sending Overview Section</div>,
-      },
-    ],
-  },
-  {
-    title: "Configuration",
-    icon: <Settings />,
-    subItems: [
-      { title: "Domain Manager", content: <div>Domain Manager Section</div> },
-    ],
-  },
-  {
-    title: "Notifications",
-    icon: <Notifications />,
-    subItems: [{ title: "Item X", content: <div>Item X Section</div> }],
-  },
-];
-
-const MenuItem: React.FC<MenuItemProps> = ({
-  title,
-  icon,
-  subItems,
-  onItemClick,
-}) => {
+const SidebarMenuItem: React.FC<MenuItemData> = ({ title, icon, subItems, path }) => {
   const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
 
   const handleClick = () => {
-    setOpen(!open);
+    if (path) {
+      navigate(path);
+    } else {
+      setOpen(!open);
+    }
   };
 
   return (
@@ -93,7 +59,7 @@ const MenuItem: React.FC<MenuItemProps> = ({
           <List component="div" disablePadding>
             {subItems.map((item, index) => (
               <NestedListItem key={index} disablePadding>
-                <ListItemButton onClick={() => onItemClick?.(item.content)}>
+                <ListItemButton onClick={() => navigate(item.path)}>
                   <ListItemText primary={item.title} />
                 </ListItemButton>
               </NestedListItem>
@@ -105,14 +71,12 @@ const MenuItem: React.FC<MenuItemProps> = ({
   );
 };
 
-const Sidebar: React.FC<{
-  onItemClick: (content: React.ReactNode) => void;
-}> = ({ onItemClick }) => {
+const Sidebar: React.FC = () => {
   return (
     <StyledDrawer variant="permanent" anchor="left">
       <List>
         {menuItems.map((item, index) => (
-          <MenuItem key={index} {...item} onItemClick={onItemClick} />
+          <SidebarMenuItem key={index} {...item} />
         ))}
       </List>
     </StyledDrawer>
--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -1,11 +1,83 @@
 import { Link } from "react-router-dom";
+import { TextField, Button, Typography, Box, Container, CssBaseline, Card, CardContent } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 export const Login = () => {
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    // Handle login logic here
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-      {/* Link to the Dashboard */}
-      <Link to="/dashboard">Go to Dashboard</Link>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '100vh',
+          }}
+        >
+          <Card elevation={5}>
+            <CardContent>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography component="h1" variant="h4" sx={{ mb: 3, fontWeight: 'bold' }}>
+                  <span style={{ color: "#1DD63A" }}>VLESIM</span> MAILER
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign In
+                  </Button>
+                  <Box sx={{ mt: 2, textAlign: 'center' }}>
+                    <Link to="/dashboard" style={{ textDecoration: 'none' }}>
+                      <Typography variant="body2" color="primary">
+                        Go to Dashboard
+                      </Typography>
+                    </Link>
+                  </Box>
+                </Box>
+              </Box>
+            </CardContent>
+          </Card>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 };
